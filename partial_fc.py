08b438b6f2d8179bfc32f0cd8a2d922ec1d70f37
Asrar <45241728+AsrarH@users.noreply.github.com>
2021-02-13 20:06:43 +0300

    Update partial_fc.py

--- partial_fc.py
+++ partial_fc.py
@@ -1,10 +1,3 @@
-"""
-Author: {Yang Xiao, Xiang An, XuHan Zhu} in DeepGlint,
-Partial FC: Training 10 Million Identities on a Single Machine
-See the original paper:
-https://arxiv.org/abs/2010.05222
-"""
-
 import argparse
 import time
 
